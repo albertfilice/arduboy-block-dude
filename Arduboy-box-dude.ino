3290a65408db6ce7b8928df221d9167c403488ca
Albert Filice <filicealbert@gmail.com>
2015-06-03 11:49:27 -0700

    Trying to get a handle on passing a two dimensional array through functions on the Arduino

--- Arduboy-box-dude.ino
+++ Arduboy-box-dude.ino
@@ -15,6 +15,22 @@
 
 Arduboy arduboy;
 
+boolean carrying = false;
+
+
+// 0 = empty
+// 1 = brick
+// 2 = box
+// 3 = door
+// 4 = dude
+
+byte levelone[5][20] = {
+  {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+  {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+  {1,0,0,0,1,0,0,0,0,0,0,0,1,0,0,0,0,0,0,1},
+  {1,0,0,0,1,0,0,0,1,0,2,0,1,0,2,0,4,0,0,1},
+  {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1}
+};
 
 void setup()
 {
@@ -23,12 +39,26 @@ void setup()
   arduboy.display();
 }
 
+void drawLevelScreen(byte level)
+{
+  switch (level)
+  {
+    case 1:
+      level = "levelone";
+      break;
+  }
+  
+  for(byte i = level[0].length; i > 0; i--)
+  {
+    arduboy.print("ok");
+  }
+}
+
 void loop()
 {
   arduboy.clearDisplay();
-  arduboy.drawBitmap(8, 8, dudeRight, 8, 8, 1);
-  arduboy.drawBitmap(8, 16, brick, 8, 8, 1);
   arduboy.display();
+  drawLevelScreen(1);
 }
 
 
