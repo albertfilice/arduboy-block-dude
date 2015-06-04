e2f4e4e519444dd5b053817f8d32587697f41192
Albert Filice <filicealbert@gmail.com>
2015-06-03 20:29:39 -0700

    Fixed the comment at the top of the main file for now

--- Arduboy-box-dude.ino
+++ Arduboy-box-dude.ino
@@ -1,13 +1,7 @@
- /*
-  Breakout
- Copyright (C) 2011 Sebastian Goscik
- All rights reserved.
-
- This library is free software; you can redistribute it and/or
- modify it under the terms of the GNU Lesser General Public
- License as published by the Free Software Foundation; either
- version 2.1 of the License, or (at your option) any later version.
- */
+/*
+  Albert Filice
+  Box Dude, or Block Dude
+*/
 
 #include <SPI.h>
 #include "Arduboy.h"
